069a2e517df85efd2448e7e6b7a322518b364d55
IraShy <60783373+IraShy@users.noreply.github.com>
2025-03-04 17:57:07 +1100

    feat(authz): implement authorized crud for entry

--- src/routers/entryRouter.js
+++ src/routers/entryRouter.js
@@ -8,17 +8,16 @@ const {
 const { findEntryById } = require("../middleware/entryMiddleware");
 
 const { Entry } = require("../models/entryModel");
-const { User } = require("../models/userModel");
 
 const router = express.Router();
 
 // @desc Find all entries
 // @route GET /entries/
-router.get("/", async (req, res) => {
+router.get("/", verifyToken, async (req, res) => {
   try {
-    res.json(await Entry.find({}));
+    res.json(await Entry.find({ author: req.user.id }));
   } catch (error) {
-    res.status(404).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 });
 
@@ -31,15 +30,15 @@ router.get(
   authorizeEntryAccess,
   (req, res) => {
     const { author, ...entryData } = req.foundEntry._doc;
-    return res.json(entryData);
+    res.json(entryData);
   }
 );
 
 // @desc Create an entry
 // @route POST /entries
-router.post("/", async (req, res) => {
+router.post("/", verifyToken, async (req, res) => {
   try {
-    const { title, description, startDate, endDate, author } = req.body;
+    const { title, description, startDate, endDate } = req.body;
 
     // TODO: check that the end date is not earlier than the start date
     // TODO: if no endDate, should it be the same as startDate or null?
@@ -49,54 +48,55 @@ router.post("/", async (req, res) => {
       description,
       startDate,
       endDate,
+      author: req.user.id,
     });
-    if (!entry) {
-      res.status(400).json({ error: "Error creating an entry" });
-    } else {
-      res.status(201).json(entry);
-    }
+
+    res.status(201).json(entry);
   } catch (error) {
     console.log(error);
+    // TODO: implement validation checks, then change the status code to 500
     res.status(400).json({ error: error.message });
   }
 });
 
 // @desc Update an entry
 // @route PUT /entries/:id
-router.put("/:id", async (req, res) => {
-  try {
-    const entry = await Entry.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    if (!entry) {
-      res.status(404).json({ error: "Entry not found" });
-    } else {
-      res.json(entry);
+router.put(
+  "/:id",
+  verifyToken,
+  findEntryById,
+  authorizeEntryAccess,
+  async (req, res) => {
+    try {
+      Object.assign(req.foundEntry, req.body);
+      await req.foundEntry.save();
+      res.json(req.foundEntry);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: error.message });
     }
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ error: error.message });
   }
-});
+);
 
 // @desc Delete an entry
 // @route DELETE /entries/:id
-router.delete("/:id", async (req, res) => {
-  try {
-    const entry = await Entry.findByIdAndDelete(req.params.id);
-    console.log(entry);
-    // const entry = await Entry.findOneAndDelete({ id: req.params.id });
-    if (!entry) {
-      res.status(404).json({ error: "Entry not found" });
-    } else {
-      res.sendStatus(200);
+router.delete(
+  "/:id",
+  verifyToken,
+  findEntryById,
+  authorizeEntryAccess,
+  async (req, res) => {
+    try {
+      const result = await Entry.deleteOne({ _id: req.params.id });
+      if (result.deletedCount === 1) {
+        res.status(200).json({ message: "Entry deleted successfully" });
+      } else {
+        res.status(404).json({ error: "Not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
-  } catch (error) {
-    res.status(400).json({ error: error.message });
   }
-});
-
-// @desc
-// @route
+);
 
 module.exports = router;
