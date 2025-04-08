bc0da9b4c59ca6a8092cbfb637867754fd7042a7
IraShy <60783373+IraShy@users.noreply.github.com>
2025-04-08 17:40:24 +1000

    chore(env): exit if JWT_KEY env variable is missing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const { app } = require("./server.js");
 const { dbConnect } = require("./config/db.js");
 
+// Check for JWT_KEY environment variable
+if (!process.env.JWT_KEY) {
+  console.error("FATAL ERROR: JWT_KEY environment variable is missing");
+  process.exit(1);
+}
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
